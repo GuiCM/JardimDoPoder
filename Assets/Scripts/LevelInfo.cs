eb344d17aaa4f8c4fd0ddb51b8e9e10116eec9d0
Guilherme CM <guilhermemilaks2@hotmail.com>
2017-08-11 15:23:01 -0300

    Parte 1 sistema de níveis[2]

---
+++ Assets/Scripts/LevelInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfo
+{
+    public int Number { get; set; }
+    public int AttackersAmount { get; set; }
+    public float AttackerTimeRateMin { get; set; }
+    public float AttackerTimeRateMax { get; set; }
+}
